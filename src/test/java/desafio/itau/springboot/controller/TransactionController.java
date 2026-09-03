9fc1a63add7c838c553d195335025102243c9339
João Victor Bueno Carneiro <114323727+dev-joaovb@users.noreply.github.com>
2026-09-03 12:09:23 -0300

    Controller criar novas transações

--- src/test/java/desafio/itau/springboot/controller/TransactionController.java
+++ src/test/java/desafio/itau/springboot/controller/TransactionController.java
@@ -16,5 +16,17 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @PostMapping
+    // Endpoint para criar uma nova transação
+    public ResponseEntity<Void> createTransaction( @Valid @RequestBody TransactionRequest resquest) {
+
+        if (request.getDataHora().isAfter(OffsetDateTime.now()) || request.getValor() <= 0) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora()));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    
 
 }
