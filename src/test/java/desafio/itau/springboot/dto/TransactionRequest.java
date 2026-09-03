cf1a4046b8d8ba22fec87b103b5f427c5459705c
João Victor Bueno Carneiro <114323727+dev-joaovb@users.noreply.github.com>
2026-09-03 11:18:04 -0300

    Primeiro Commit Service

---
+++ src/test/java/desafio/itau/springboot/dto/TransactionRequest.java
@@ -0,0 +1,23 @@
+package desafio.itau.springboot.dto;
+
+import java.time.OffsetDateTime;
+import jakarta.validation.constraints.min;
+import jakarta.validation.constraints.NotNull;
+
+public class TransactionRequest {
+
+    @NotNull
+    @Min(0)
+    private double valor;
+
+    @NotNull
+    private OffsetDateTime dataHora;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+}
