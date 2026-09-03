b3297ff41e5844a5ae4efd4c08eff7bf91836a7f
João Victor Bueno Carneiro <114323727+dev-joaovb@users.noreply.github.com>
2026-09-03 12:34:08 -0300

    Configurações finais

--- src/test/java/desafio/itau/springboot/dto/TransactionRequest.java
+++ src/test/java/desafio/itau/springboot/dto/TransactionRequest.java
@@ -1,14 +1,12 @@
 package desafio.itau.springboot.dto;
 
 import java.time.OffsetDateTime;
-import jakarta.validation.constraints.min
-;
 import jakarta.validation.constraints.NotNull;
 
 public class TransactionRequest {
 
     @NotNull
-    @Min(value = 0, message = "O valor da transação deve ser um número positivo")
+//    @Min(value = 0, message = "O valor da transação deve ser um número positivo")
     private double valor;
 
     @NotNull
